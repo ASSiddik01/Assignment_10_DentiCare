2debd62e74d25b69260db9d04583a5d071d54b06
Shama <assiddik001@gmail.com>
2022-04-17 14:36:59 +0600

    Implement social login

--- src/Pages/Shared/User/SocialLogin/SocialLogin.js
+++ src/Pages/Shared/User/SocialLogin/SocialLogin.js
@@ -2,8 +2,41 @@ import React from "react";
 import "./SocialLogin.css";
 import google from "../../../../images/social/google.png";
 import github from "../../../../images/social/github.png";
+import {
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
+import auth from "../../../../firebase.config";
+import Loading from "../../Loading/Loading";
 
 const SocialLogin = () => {
+  const [signInWithGoogle, googleUser, googleLoading, googleError] =
+    useSignInWithGoogle(auth);
+  const [signInWithGithub, githubUser, githubloading, githubError] =
+    useSignInWithGithub(auth);
+  const navigate = useNavigate();
+  let location = useLocation();
+
+  let from = location.state?.from?.pathname || "/";
+
+  let errorElement;
+
+  if (googleError || githubError) {
+    errorElement = (
+      <p className="text-danger">
+        Error: {googleError?.message} {githubError?.message}
+      </p>
+    );
+  }
+
+  if (googleLoading || githubloading) {
+    return <Loading></Loading>;
+  }
+
+  if (googleUser || githubUser) {
+    navigate(from, { replace: true });
+  }
   return (
     <div>
       <div className="d-flex theme_color">
@@ -11,12 +44,23 @@ const SocialLogin = () => {
         <p>or</p>
         <hr style={{ width: "50%" }} />
       </div>
+      {errorElement}
       <div className=" d-flex justify-content-evenly ">
-        <button type="submit" className="btn btn-outline-themeButton">
-          <img className="px-2" src={google} alt="" />Sign in with Google
+        <button
+          onClick={() => signInWithGoogle()}
+          type="submit"
+          className="btn btn-outline-themeButton"
+        >
+          <img className="px-2" src={google} alt="" />
+          Sign in with Google
         </button>
-        <button type="submit" className="btn btn-outline-themeButton">
-          <img className="px-2" src={github} alt="" />Sign in with Google
+        <button
+          onClick={() => signInWithGithub()}
+          type="submit"
+          className="btn btn-outline-themeButton"
+        >
+          <img className="px-2" src={github} alt="" />
+          Sign in with Google
         </button>
       </div>
     </div>
